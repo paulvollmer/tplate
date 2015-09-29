db67e6e56ec2cc6fd0aa5102403f2456be069c64
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-09-29 22:30:57 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,60 +1,91 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
-	"text/template"
+	"path/filepath"
 )
 
-// some defines...
-// var defines = `
-// {{define "func"}}func(){}{{end}}
-//
-// {{define "main"}}func main(){
-// 	(.arg1)
-// 	}{{end}}
-// `
+var (
+	version string // the current version
+	gitRev  string // the git revision hash (set by the Makefile)
 
-// run the application.
+	EnvVarPath        string // the TPLATE_PATH environment var
+	EnvVarAuthor      string // the TPLATE_AUTHOR environment var
+	EnvVarAuthorEmail string // the TPLATE_AUTHOR_EMAIL environment var
+)
+
 func main() {
-	// variable to store the source string.
-	src := ""
+	// get the environment variables
+	EnvVarPath = os.Getenv("TPLATE_PATH")
+	EnvVarAuthor = os.Getenv("TPLATE_AUTHOR")
+	if EnvVarAuthor == "" {
+		EnvVarAuthor = "tplate"
+	}
+	EnvVarAuthorEmail = os.Getenv("TPLATE_AUTHOR_EMAIL")
 
-	// parse the commandline args and read file.
-	if len(os.Args) == 1 {
-		println("missing source...")
-		println("try $ tplate source.tpl")
+	// define and parse commandline flags
+	flagEnv := flag.Bool("env", false, "the environment vars")
+	flagList := flag.Bool("list", false, "list all templates")
+	flagHelp := flag.Bool("help", false, "show the help")
+	flagVersion := flag.Bool("version", false, "print the version")
+	flag.Parse()
+
+	if *flagEnv {
+		PrintEnvVars()
+	} else if *flagList {
+		fmt.Printf("list directory '%s'\n", EnvVarPath)
+		ListFiles(EnvVarPath, "")
+	} else if *flagHelp {
+		Help()
+	} else if *flagVersion {
+		fmt.Println(version + " rev: " + gitRev)
 	} else {
-		filepath := os.Args[1] + ".tplate"
-		dat, err := ioutil.ReadFile(filepath)
-		if err != nil {
-			panic(err)
+		// parse the commandline args and read file.
+		totalArgs := len(os.Args)
+		if totalArgs == 1 {
+			fmt.Println("missing template source!")
+			fmt.Println("check out the tplate -help for more information")
+			os.Exit(1)
+		} else {
+			tmpFilepath := EnvVarPath + os.Args[1] + ".tplate"
+			tmpVars := []string{}
+			if totalArgs > 2 {
+				tmpVars = os.Args[2:]
+			}
+			result, err := Process(tmpFilepath, tmpVars)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
+			}
+			fmt.Print(string(result))
 		}
-		src = string(dat)
 	}
+}
 
-	/*funcMap := template.FuncMap{
-	  "Get": func() string {
-	    return "--> get..."
-	  },
-	}*/
-	tmpl, errTmpl := template.New("tpl"). /*.Funcs(funcMap)*/ Parse( /*defines +*/ src + "\n")
-	if errTmpl != nil {
-		panic(errTmpl)
-	}
+func PrintEnvVars() {
+	fmt.Printf("TPLATE_PATH         = %s\n", EnvVarPath)
+	fmt.Printf("TPLATE_AUTHOR       = %s\n", EnvVarAuthor)
+	fmt.Printf("TPLATE_AUTHOR_EMAIL = %s\n", EnvVarAuthorEmail)
+}
 
-	tmplData := TemplateData{Name: "Mary"}
-	err := tmpl.Execute(os.Stdout, tmplData)
+func ListFiles(src, prefix string) {
+	tmp, err := ioutil.ReadDir(src)
 	if err != nil {
-		log.Printf("execution failed: %s", err)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _, file := range tmp {
+		if file.IsDir() {
+			ListFiles(src+string(filepath.Separator)+file.Name(), file.Name()+string(filepath.Separator))
+		} else {
+			fmt.Println(prefix + file.Name())
+		}
 	}
 }
 
-type TemplateData struct {
-	Name string
-}
-
-func (t TemplateData) Yo(name string) string {
-	return "Yo " + name
+func Help() {
+	flag.Usage()
 }
